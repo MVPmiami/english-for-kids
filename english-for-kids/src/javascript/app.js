d16c1efb6c1b08c8b99443a71894e61597a97d84
Aliaksei Siankevich <andfly@tut.by>
2020-12-06 19:23:43 +0300

    add: add logo of  school

--- english-for-kids/src/javascript/app.js
+++ english-for-kids/src/javascript/app.js
@@ -97,4 +97,14 @@ export {isNewPage};
 export {currentCard};
 
 
+var User = function() {};
+
+User.prototype.attributes = {isAdmin: false};
+
+var admin = new User('Sam'), guest = new User('Bob');
+
+admin.attributes.isAdmin = true;
+console.log(admin.attributes.isAdmin);
+console.log(guest.attributes.isAdmin);
+
 
